5e82210db86ac56f231aa5e394c5b817c27c6448
JZL <353553339@qq.com>
2021-04-29 16:07:15 +0800

    记录日志时支持格式化输出

--- logger/console.go
+++ logger/console.go
@@ -25,38 +25,39 @@ func (l Logger) enable(logLevel Loglevel) bool {
 	return logLevel >= l.Level
 }
 
-func log(lv Loglevel, msg string) {
+func log(lv Loglevel, format string, a ...interface{}) {
+	msg := fmt.Sprintf(format, a...)
 	now := time.Now()
 	funcName, fileName, lineNo := getInfo(3)
 	fmt.Printf("[%s] [%s] [%s:%s:%d] %s\n", now.Format("2006-01-02 15:04:05"), getLogString(lv), fileName, funcName, lineNo, msg)
 }
 
-func (l Logger) Debug(msg string) {
+func (l Logger) Debug(format string, a ...interface{}) {
 	if l.enable(DEBUG) {
-		log(DEBUG, msg)
+		log(DEBUG, format, a...)
 	}
 }
 
-func (l Logger) Info(msg string) {
+func (l Logger) Info(format string, a ...interface{}) {
 	if l.enable(INFO) {
-		log(INFO, msg)
+		log(INFO, format, a...)
 	}
 }
 
-func (l Logger) Warning(msg string) {
+func (l Logger) Warning(format string, a ...interface{}) {
 	if l.enable(WARNING) {
-		log(WARNING, msg)
+		log(WARNING, format, a...)
 	}
 }
 
-func (l Logger) Error(msg string) {
+func (l Logger) Error(format string, a ...interface{}) {
 	if l.enable(ERROR) {
-		log(ERROR, msg)
+		log(ERROR, format, a...)
 	}
 }
 
-func (l Logger) Fatal(msg string) {
+func (l Logger) Fatal(format string, a ...interface{}) {
 	if l.enable(FATAL) {
-		log(FATAL, msg)
+		log(FATAL, format, a...)
 	}
 }
--- run/main.go
+++ run/main.go
@@ -12,7 +12,9 @@ func main() {
 		log.Debug("这是一条Debug日志")
 		log.Info("这是一套Info日志")
 		log.Warning("这是一套Warning日志")
-		log.Error("这是一套Error日志")
+		id := 10001
+		name := "xss"
+		log.Error("这是一套Error日志%v %v", id, name)
 		log.Fatal("这是一套Fatal日志")
 		time.Sleep(time.Second)
 	}
